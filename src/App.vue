290bd8c144da18cf6b04b754a4aae1972f6b5d15
lizhigao <lizhigao@021.com>
2021-01-18 08:57:51 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- <Demo1></Demo1> -->
+  <Demo2></Demo2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import Demo1 from './Demo1.vue';
+import Demo2 from './Demo2.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    // Demo1,
+    Demo2,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
